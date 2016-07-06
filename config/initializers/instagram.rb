980c0118f03140337ad524587b0a7f548d65ea80
browenchen <owenchen@berkeley.edu>
2016-07-06 02:21:02 -0700

    insta hash

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,9 @@
+require "instagram"
+
+Instagram.configure do |config| 
+	
+config.client_id = "d0fd91625af640d69b5fbbad3334bbff"
+config.access_token = "2075097003.d0fd916.2e005b78e29d4040857089c771ca88b1"
+
+end
+
